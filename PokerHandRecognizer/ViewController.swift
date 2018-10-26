31e94f77e00b7fb62741b0b7c13d27ffd2c01c6a
Ryan Gaines <ryan.gaines@uky.edu>
2018-10-26 11:40:09 -0400

    changed Xcode files

--- PokerHandRecognizer/ViewController.swift
+++ PokerHandRecognizer/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import SceneKit
+import ARKit
 
-class ViewController: UIViewController {
+import Vision
 
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.setUpARKit()
+        self.setUpVision()
+    }
+    
+    func setUpARKit(){
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
+        // Enable Default Lighting - makes the 3D text a bit poppier.
+        sceneView.autoenablesDefaultLighting = true
+    }
+    
+    func setUpVision(){
+
+        
     }
 
 
